0c06176df1949befa0dbb0c2dcbffcfcb70f5566
DESKTOP-5Q49QCU\PC <a151815058@gmail.com>
2023-02-26 17:14:19 +0800

    add new page

--- app.py
+++ app.py
@@ -4,15 +4,23 @@ from flask import  request
 
 app = Flask(__name__)
 
-
+@app.route('/news')
 @app.route('/')
 def start():  # put application's code here
-    return render_template('index.html')
+    return render_template('news.html')
+
 
 @app.route('/album')
 def album():  # put application's code here
     return render_template('album.html')
 
+@app.route('/about')
+def about():  # put application's code here
+    return render_template('about.html')
+
+@app.route('/history')
+def history():  # put application's code here
+    return render_template('history.html')
 
 if __name__ == '__main__':
     app.run()
