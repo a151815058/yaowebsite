c66c7e1c917ae2f5e5348051c3dd2bf1b946678c
姚佩彤 <a151815058@gmail.com>
2023-02-28 16:35:11 +0800

    add func

--- app.py
+++ app.py
@@ -1,36 +1,41 @@
 from flask import Flask
 from flask import render_template
+from flask import redirect
 from flask import  request
 
 app = Flask(__name__)
 
-@app.route('/news')
-@app.route('/')
-def start():  # put application's code here
-    return render_template('news.html')
+@app.route('/', methods=['GET', 'POST'])
+def index():  # put application's code here
+    return render_template('index.html',index=False)
 
+@app.route('/news', methods=['GET', 'POST'])
+def news():  # put application's code here
+    return render_template('news.html',index=False)
 
-@app.route('/album')
+@app.route('/album', methods=['GET', 'POST'])
 def album():  # put application's code here
-    return render_template('album.html')
+    return render_template('album.html',index=False)
 
-@app.route('/about')
+@app.route('/about', methods=['GET', 'POST'])
 def about():  # put application's code here
-    return render_template('about.html')
+    return render_template('about.html',index=False)
 
-@app.route('/history')
+@app.route('/history', methods=['GET', 'POST'])
 def history():  # put application's code here
-    return render_template('history.html')
+    return render_template('history.html',index=False)
 
-@app.route('/blog')
+@app.route('/blog', methods=['GET', 'POST'])
 def blog():  # put application's code here
-    return render_template('blog.html')
+    return render_template('bulletin.html',index=False)
 
-@app.route('/organizational_chart')
+@app.route('/organizational_chart', methods=['GET', 'POST'])
 def organizational_chart():  # put application's code here
-    return render_template('organizational_chart.html')
+    return render_template('organizational_chart.html',index=False)
 
 
 
 if __name__ == '__main__':
-    app.run()
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.jinja_env.auto_reload = True
+    app.run(debug=True)
